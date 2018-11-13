c77aba84a18968a5fcdddcfe09e4c64a99d45dd8
risch1997 <risch1997@aol.com>
2018-11-13 12:24:26 -0500

    Added Login and Logout functionality

--- includes/header.php
+++ includes/header.php
@@ -1,3 +1,14 @@
 <?php
+	session_start();
 	include_once("../functions/load.php");
+	
+	if (isset($_POST['logout'])) {
+		logout();
+	}
 ?>
+
+<html>
+	<head>
+		<title>Study Hall</title>
+	</head>
+	<body>
