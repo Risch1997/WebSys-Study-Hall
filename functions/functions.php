a9118c48d9e973870e9c0a9c1c86ce0e1ed6e3f7
Huanzhi Zhu <justicepigzhz@gmail.com>
2018-11-28 19:40:35 -0500

    modified functions naming and etc.

--- functions/functions.php
+++ functions/functions.php
@@ -5,8 +5,8 @@
 	include("functions_user.php");
 
 	// EDITED: hzz, Nov 25
-	include("function_screlate.php");
-	include("function_match.php");
+	include("functions_screlate.php");
+	include("functions_match.php");
 
 	function dbConnect($selectDB = true) {
 		global $dbConfig, $dbConn;
