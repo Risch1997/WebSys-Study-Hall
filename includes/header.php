2c6bac247cb9bdb8c114948617386669f0e97bc7
risch1997 <risch1997@aol.com>
2018-12-03 21:17:40 -0500

    Changed browsing page to show card for all students, allows users to send invites to users.

--- includes/header.php
+++ includes/header.php
@@ -7,12 +7,45 @@
 		header("Location: ../pages/login.php");
 	}
 ?>
-
+<!DOCTYPE html>
 <html>
-	<head>
-		<title>Study Hall</title>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js" type="text/javascript"></script>
-		<style><?php include '../styles/editProfile.css'; ?></style>
-		<style><?php include '../styles/studyHall.css'; ?></style>
-	</head>
-	<body>
+  <head>
+    <title>Web Systems and Development: Study Hall</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <link rel="stylesheet" href="../styles/navbar.css">
+    <link rel="stylesheet" href="../styles/studyHall.css">
+    <link rel="stylesheet" href="../styles/editProfile.css">
+    <link rel="stylesheet" href="../styles/browse.css">
+    <script src="../scripts/studyHall.js" type="text/javascript" charset="utf-8" async defer></script>
+    <script src="https://code.jquery.com/jquery-1.9.1.min.js"></script>
+    <style type="text/css">
+      body {
+        font-family: Arial, Helvetica, sans-serif;
+      }
+    </style>
+  </head>
+  <body>
+    <div style="width: 100%" id = "header">
+      <div >
+        <hr>
+        <div id="logo">
+          <a href="index.php">
+            <img src="../studyHallLogo.png" alt="Logo for the Study Hall Site" height="100">
+          </a>
+        </div>
+        <br />
+        <div>
+          <ol>
+            <li><a href="browse.php">Browse for Matches</a></li>
+            <li><a href="">My Matches</a></li>
+            <li><a href="editProfile.php">My Profile</a></li>
+            <li><a href="logout.php">Log Out</a></li>
+          </ol>
+          <form id="logout" method="POST">
+            <input type="submit" name="logout" value="Log Out">
+          </form>
+        </div>
+        <br />
+        <hr>
+      </div>
+    </div>
